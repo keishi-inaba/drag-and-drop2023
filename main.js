5a1698d7d3128239ac6734faaf86dcf4751fc50e
keishi.i <gajyumaru1103@gmail.com>
2023-05-12 17:26:39 +0900

    done

---
+++ main.js
@@ -0,0 +1,51 @@
+// 1.要素取得
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+// console.log(empties);
+
+// 2.ドラッグのトリガー
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+// 4.1つ1つのempty要素を取得
+for(const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+// 3.ドラッグ関数
+function dragStart() {
+  console.log("start");
+  fill.className += " hold";
+  setTimeout(() => {
+    fill.className = "invisible";
+  }, 0);
+}
+
+function dragEnd() {
+  console.log("start");
+  fill.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  console.log("over");
+}
+
+function dragEnter() {
+  console.log("enter");
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  console.log("leave");
+  this.className = "empty";
+}
+
+function dragDrop() {
+  console.log("drop");
+  this.className = "empty";
+  this.appendChild(fill);
+}
